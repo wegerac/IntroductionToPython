455ea2a9548bcbc6a97e0e6fed8509488a3c2fa0
wegerac <wegerac@rose-hulman.edu>
2017-11-27 16:37:07 -0500

    Completed all the TODO's

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -73,11 +73,25 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
+matt.speed = 5
+matt.pen = rg.Pen('blue', 20)
+matt.left(160)
+matt.forward(200)
 
+dave.pen = rg.Pen('purple', 10)
+dave.right(90)
+dave.forward(50)
+dave.right(90)
+dave.forward(400)
+dave.pen = rg.Pen('green', 20)
+dave.right(90)
+dave.forward(270)
+dave.right(90)
+dave.forward(60)
 
 ########################################################################
 #
-# TODO: 3.
+#  DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +105,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +123,20 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+rosie = rg.SimpleTurtle('turtle')
+rosie.pen = rg.Pen('yellow', 60)
+rosie.right(90)
+rosie.forward(300)
+rosie.left(135)
+rosie.forward(160)
+rosie.left(45)
+rosie.forward(300)
+rosie.left(135)
+rosie.forward(160)
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
